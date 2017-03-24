e3a76c41d7bc60f9c839018a211f3a10a41e5270
florian lahitte <lahitte.florian@gmail.com>
2017-03-24 12:48:11 +0100

    Add some style for concept

--- assets/js/dependencies/homepages.js
+++ assets/js/dependencies/homepages.js
@@ -21,8 +21,12 @@ $( document ).ready(function() {
           });
           //for animation move right and left
           window.sr = ScrollReveal({ reset: true });
-          sr.reveal('.mov_left', { duration: 1000, origin: 'left', mobile: true });
-          sr.reveal('.mov_right', { duration: 1000, origin: 'right', mobile: true });
+          sr.reveal('.mov_left', { duration: 1500, origin: 'left',distance: '150px', mobile: true });
+          sr.reveal('.mov_right', { duration: 1500, origin: 'right',distance: '150px', mobile: true });
+          sr.reveal('.mov_left2', { duration: 1500, origin: 'left',distance: '150px',delay: 800, mobile: true });
+          sr.reveal('.mov_right2', { duration: 1500, origin: 'right',distance: '150px',delay: 800, mobile: true });
+          sr.reveal('.mov_up', { duration: 1000, origin: 'bottom',distance: '150px', delay: 1500, easing: 'linear', mobile: true });
+
 
     //for toogle button
     $(".button-prestation").click(function (e) {
@@ -31,6 +35,6 @@ $( document ).ready(function() {
 
        $('[class^=show-presta]').not($('.'+this.id)).hide();
 
-       $('.'+this.id).slideToggle(500);
+       $('.'+this.id).show(1000);
 })
 });
